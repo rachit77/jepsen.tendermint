2c7f1dae03736528a160b5ca60c1b546824e205a
Rachit Sonthalia <rachitsonthalia02@gmail.com>
2023-03-14 15:03:38 +0530

    updated tendermint binary link

--- src/jepsen/tendermint/cli.clj
+++ src/jepsen/tendermint/cli.clj
@@ -6,9 +6,9 @@
 
 (def opts
   "Extra command line opts."
-  [(jc/package-opt "tendermint-url" "https://github.com/tendermint/tendermint/archive/refs/tags/v0.34.23.tar.gz")])
+  [(jc/package-opt "tendermint-url" "https://github.com/tendermint/tendermint/releases/download/v0.34.22/tendermint_0.34.22_linux_amd64.tar.gz")])
 
-
+                                   
 (defn -main
   "Handles command line arguments. And run a test."
   [& args]
